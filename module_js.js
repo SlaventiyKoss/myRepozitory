2d7e5acc2346fa967fdc78d506bb667d23301d36
iSlaventiy <kostaslava@gmail.com>
2018-01-24 23:36:48 +0200

    first commit

--- module_js.js
+++ module_js.js
@@ -2,16 +2,40 @@
 
 var arr = ['1', 0, Math.random, alert, prompt, prompt, null, undefined, "name", {}];
 function typeStats(arr) {
-	initial = {}
+	var obj = {} 
 	arr.map(element => {
-    	if (! initial[typeof element]) {
-       		initial[typeof element] = 1
+    	if (! obj[typeof element]) {
+       		obj[typeof element] = 1
     		}
-    		else initial[typeof element]++;
+    		else obj[typeof element]++;
   		})
-  	return initial
+  	return obj
 }
 typeStats(arr);
+
+=========
+
+var arr = ['1', 0, Math.random, alert, prompt, prompt, null, undefined, "name", {}];
+function typeStats(array) {
+  var obj = new Object();
+  for(i in array){
+    if (! obj[typeof array[i]]){
+      obj[typeof array[i]] = 1;
+    }
+    else {
+      obj[typeof array[i]] ++;
+    }
+  }
+  return obj;
+}
+typeStats(arr);
+
+for (i in arr){
+  debugger;
+  console.log(typeof arr[i]);
+}
+
+
 
 // Intersect (2)
 
@@ -28,13 +52,31 @@ var a = [1,2,3,4]
 var b = [3,4,5,6]
 intersect(a,b);
 
+==================
+
+function intersect(a, b) {
+  var arr = [];
+  a.forEach(function(el1){
+    if (b.some(function(el2){
+      return el2 == el1;
+    })){arr.push(el1);}
+  });
+  return arr;
+}
+var a = [1,2,3,4]
+var b = [3,4,5,6]
+intersect(a,b);
+
+
+
+
 // oddEvenClass (3)
 
-function oddEvenClass(querySet, class1, class2) {
-	var _isOdd = false;
-	for (var child of querySet.children) {
-    	_isOdd ? child.classList.add(class1) : child.classList.add(class2);
-    	_isOdd = !_isOdd;
+function oddEvenClass(element, class1, class2) {
+	var step = false;
+	for (var child of element.children) {
+    	step ? child.className = class1 : child.className = class2;
+    	step = !step;
 	}
 }
 var table = document.querySelector('#table tbody')
